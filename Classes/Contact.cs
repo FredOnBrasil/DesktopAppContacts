ef9c59265b002c3e6b6e9771298c77873fe7db8c
FredOnBrasil <76821162+FredOnBrasil@users.noreply.github.com>
2021-02-09 17:07:43 -0300

    Add files via upload

---
+++ Classes/Contact.cs
@@ -0,0 +1,28 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopContactsApp.Classes
+{
+    public class Contact
+    {
+        //defining the primarykey ID to the class/table contacts
+        //definig the atribute primarykey to ID ! and its autoincrement
+        [PrimaryKey, AutoIncrement] 
+        public int Id { get; set; }
+
+        //implicit definition of properties in the class contact        
+        public string Name { get; set; } 
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        //overriding the return of get method in this class to return the correct data on the listview
+        public override string ToString()
+        {
+            return $"{Id} -Name: {Name} - Phone: {Phone} - Email: {Email}";
+        }
+    }
+}
