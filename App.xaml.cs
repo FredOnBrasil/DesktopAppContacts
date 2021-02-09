b7eed5ce2196abe5255a2369458d9ba0a30ce011
FredOnBrasil <76821162+FredOnBrasil@users.noreply.github.com>
2021-02-09 17:04:42 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // more eficient definition of variables we are using in the application
+        // making the information about database to this application
+        // ps: databaseName -/- folderPath accessible only in this local (good pratice)
+        static string databaseName = "Contacts.db";
+        static string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        public static string databasePath = System.IO.Path.Combine(folderPath, databaseName);
+    }
+}
---
+++ ContactsDetailsWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for ContactsDetailsWindow.xaml
+    /// </summary>
+    public partial class ContactsDetailsWindow : Window
+    {
+        // global variable to receive the contact
+        Contact contact;
+
+        //implements the need for pass a contact whenever we implement a new contactsdetailwindow
+        public ContactsDetailsWindow(Contact contact)
+        {
+            InitializeComponent();
+
+            // initialize contact
+            this.contact = contact;
+
+            // to populate every textboxat this window:
+            nameTextBox.Text = contact.Name;
+            emailTextBox.Text = contact.Email;
+            phoneTextBox.Text = contact.Phone; 
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            // create the conection to database and open it /--- the using statement give the permition to use the dispose to
+            // use the sonnection until the end of this litle block of code, and closes the connection automatically
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                // verify the table(if don't exists create it) using generics to generate the table contact is called only to create the table
+                connection.CreateTable<Contact>();
+
+                // delete the contact into the table Contact
+                connection.Delete(contact);
+            }
+
+            Close();
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            // to update the data present at this window to contacts in the database:
+            contact.Name = nameTextBox.Text;
+            contact.Email = emailTextBox.Text;
+            contact.Phone = phoneTextBox.Text;
+
+            // create the conection to database and open it /--- the using statement give the permition to use the dispose to
+            // use the sonnection until the end of this litle block of code, and closes the connection automatically
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                // verify the table(if don't exists create it) using generics to generate the table contact is called only to create the table
+                connection.CreateTable<Contact>();
+
+                // update the contact into the table Contact
+                connection.Update(contact);
+            }
+
+            Close();
+        }
+    }
+}
